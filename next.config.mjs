326baffe9e457ba1df23040fa9e316fecebcd4d4
CodzHorizon <kshitizsh.8@gmail.com>
2025-07-18 17:56:43 +0530

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,94 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            'encrypted-tbn2.gstatic.com',  // Google thumbnail
-            'images.unsplash.com',           // Unsplash
-            'cdn.sanity.io',                 // Sanity CMS
-            'res.cloudinary.com',            // Cloudinary CDN
-            'pbs.twimg.com',                 // Twitter profile images
-            'avatars.githubusercontent.com',// GitHub avatars
-            'lh3.googleusercontent.com',    // Google user profile pics
-            'media.discordapp.net',          // Discord media
-            'cdn.discordapp.com',            // Discord CDN
-            'i.imgur.com',                   // Imgur
-            'cdn.shopify.com',               // Shopify CDN
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn*.gstatic.com', // Using wildcard for gstatic subdomains
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placekitten.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'link-hub-two.vercel.app',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
